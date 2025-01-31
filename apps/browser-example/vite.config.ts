7f08d3c50aa13ceeef1b333253a8d0bfd1fbc82b
Scott Willeke <scott@willeke.com>
2025-01-31 11:16:45 -0800

    chore: build cleanup for a clean install and build

--- apps/browser-example/vite.config.ts
+++ apps/browser-example/vite.config.ts
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite"
+import react from "@vitejs/plugin-react"
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  css: {
+    // https://vite.dev/config/shared-options.html#css-preprocessoroptions
+    preprocessorOptions: {
+      scss: {
+        // https://sass-lang.com/documentation/js-api/interfaces/options/#quietDeps
+        quietDeps: true,
+      },
+    },
+  },
 })
